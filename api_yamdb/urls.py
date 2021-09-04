5be375d1a71d22a8a79ac4ebda7768c02397af14
Vladimir <clawfinger2008@yandex.ru>
2021-09-04 21:49:08 +0300

    redirect from mainpage to redoc

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path(
+        '',
+        RedirectView.as_view(url='redoc', permanent=True),
+        name='index'
+    ),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path(
