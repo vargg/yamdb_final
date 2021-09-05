3d4f719e9142f4d0d9a6f23ae42fefbd1ee31ac9
Vladimir <clawfinger2008@yandex.ru>
2021-09-05 11:53:02 +0300

    fix redoc again

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'corsheaders',
     'django_filters',
+    'drf_yasg',
     'api',
 ]
 
--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
 from django.views.generic.base import RedirectView
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 urlpatterns = [
     path(
@@ -11,9 +13,22 @@ urlpatterns = [
     ),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+
+]
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="YaMDb API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
     path(
         'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
+        schema_view.with_ui('redoc', cache_timeout=0),
         name='redoc'
     ),
 ]
